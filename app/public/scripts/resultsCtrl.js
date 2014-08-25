96e5eb72df42b5f035318aaf6dfad210c0c8d7b6
Gaël Métais <gael.metais@gmail.com>
2014-08-25 17:31:07 +0200

    Improve Phantomas metrics reporting

--- app/public/scripts/resultsCtrl.js
+++ app/public/scripts/resultsCtrl.js
@@ -3,12 +3,15 @@ var app = angular.module('Results', []);
 app.controller('ResultsCtrl', function ($scope) {
     // Grab results from nodeJS served page
     $scope.phantomasResults = window._phantomas_results;
+    $scope.phantomasMetadata = window._phantomas_metadata.metrics;
 
     $scope.view = 'summary';
     $scope.slowRequestsOn = false;
     $scope.slowRequestsLimit = 5;
 
     if ($scope.phantomasResults.offenders && $scope.phantomasResults.offenders.javascriptExecutionTree) {
+        
+        // Get the execution tree from the offenders
         $scope.javascript = JSON.parse($scope.phantomasResults.offenders.javascriptExecutionTree);
     
         // Read the main elements of the tree and sum the total time
@@ -23,6 +26,20 @@ app.controller('ResultsCtrl', function ($scope) {
                 return false;
             }
         });
+    }
+
+    if ($scope.phantomasResults.metrics && $scope.phantomasResults.offenders) {
+
+        // Get the Phantomas modules from metadata
+        $scope.metricsModule = {};
+        for (var metricName in $scope.phantomasMetadata) {
+            var metric = $scope.phantomasMetadata[metricName];
+            if (!$scope.metricsModule[metric.module]) {
+                $scope.metricsModule[metric.module] = {};
+            }
+            $scope.metricsModule[metric.module][metricName] = metric;
+        }
+
     }
 
     $scope.setView = function(viewName) {
