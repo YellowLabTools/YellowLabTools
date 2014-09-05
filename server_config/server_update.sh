831d26710667bde2dafeaa6511b582a2f2cd9a55
Gaël Métais <gael.metais@gmail.com>
2014-09-05 16:12:53 +0200

    Add bower and npm update in update script

--- server_config/server_update.sh
+++ server_config/server_update.sh
@@ -10,5 +10,9 @@ git stash
 git pull
 git stash pop
 
+# In case something was added in package.json or bower.json
+npm install --production
+bower install --config.interactive=false --allow-root
+
 # Restart the server
 forever start -c "node --stack-size=65500" server.js
