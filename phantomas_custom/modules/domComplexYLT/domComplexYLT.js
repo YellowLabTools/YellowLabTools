2714b2bd117aadcec7bb569afec440e76b2b91f1
Gaël Métais <gael.metais@gmail.com>
2014-09-17 13:22:05 +0200

    Update Phantomas version

--- phantomas_custom/modules/domComplexYLT/domComplexYLT.js
+++ phantomas_custom/modules/domComplexYLT/domComplexYLT.js
@@ -3,16 +3,13 @@
  */
 /* global document: true, Node: true, window: true */
 
-exports.version = '0.3.a';
+exports.version = '0.4.a';
 
 exports.module = function(phantomas) {
     'use strict';
 
     // total length of HTML comments (including <!-- --> brackets)
     phantomas.setMetric('commentsSize'); // @desc the size of HTML comments on the page @offenders
-
-    // total length of HTML of hidden elements (i.e. display: none)
-    phantomas.setMetric('hiddenContentSize'); // @desc the size of content of hidden elements on the page (with CSS display: none) @offenders
 
     // total length of text nodes with whitespaces only (i.e. pretty formatting of HTML)
     phantomas.setMetric('whiteSpacesSize'); // @desc the size of text nodes with whitespaces only
@@ -74,24 +71,6 @@ exports.module = function(phantomas) {
 
                             // ignore inline <script> tags
                             if (node.nodeName === 'SCRIPT') {
-                                return false;
-                            }
-
-                            // @see https://developer.mozilla.org/en/DOM%3awindow.getComputedStyle
-                            var styles = window.getComputedStyle(node);
-
-                            if (styles && styles.getPropertyValue('display') === 'none') {
-                                if (typeof node.innerHTML === 'string') {
-                                    size = node.innerHTML.length;
-                                    phantomas.incrMetric('hiddenContentSize', size);
-
-                                    // log hidden containers bigger than 1 kB
-                                    if (size > 1024) {
-                                        phantomas.addOffender('hiddenContentSize', phantomas.getDOMPath(node) + ' (' + size + ' characters)');
-                                    }
-                                }
-
-                                // don't run for child nodes as they're hidden as well
                                 return false;
                             }
 
