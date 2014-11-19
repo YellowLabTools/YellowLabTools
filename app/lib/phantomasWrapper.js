d84b831922cd574d6961c75e3b6c38ebabd3b8df
Gaël Métais <gael.metais@gmail.com>
2014-11-19 19:33:48 +0100

    #1 Change user agent to simulate Chrome

--- app/lib/phantomasWrapper.js
+++ app/lib/phantomasWrapper.js
@@ -22,6 +22,7 @@ var PhantomasWrapper = function() {
             // Cusomizable options
             timeout: task.options.timeout || 60,
             'js-deep-analysis': task.options.jsDeepAnalysis || false,
+            'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.110 Safari/537.36',
 
             // Mandatory
             reporter: 'json:pretty',
