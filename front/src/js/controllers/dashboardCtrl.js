486ede9396b25da906400eddedc08f1631ca58ce
Gaël Métais <gael.metais@gmail.com>
2015-04-25 18:38:41 +0200

    Promote the Grunt plugin

--- front/src/js/controllers/dashboardCtrl.js
+++ front/src/js/controllers/dashboardCtrl.js
@@ -59,10 +59,5 @@ dashboardCtrl.controller('DashboardCtrl', ['$scope', '$rootScope', '$routeParams
         window.open(url, 'sharer', 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width=' + winWidth + ',height=' + winHeight);
     }
 
-    // Returns the URL of the JSON result
-    $scope.getAPIUrl = function() {
-        return '/api/results/' + $scope.runId;
-    };
-
     loadResults();
 }]);
