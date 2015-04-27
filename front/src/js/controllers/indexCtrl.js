cae23d61e264fe59c6e039d5eb727053a3169e45
Gaël Métais <gael.metais@gmail.com>
2015-04-27 15:40:46 +0200

    Add a device selector on the launching page

--- front/src/js/controllers/indexCtrl.js
+++ front/src/js/controllers/indexCtrl.js
@@ -1,8 +1,13 @@
 var indexCtrl = angular.module('indexCtrl', []);
 
-indexCtrl.controller('IndexCtrl', ['$scope', '$location', 'API', function($scope, $location, API) {
+indexCtrl.controller('IndexCtrl', ['$scope', 'Settings', 'API', function($scope, Settings, API) {
+    
+    $scope.settings = Settings.getMergedSettings();
+    console.log($scope.settings);
+
     $scope.launchTest = function() {
         if ($scope.url) {
+            Settings.saveSettings($scope.settings);
             API.launchTest($scope.url);
         }
     };
