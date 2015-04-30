b2f6f68d1abbea98766eb9894ca8001c60152352
Gaël Métais <gael.metais@gmail.com>
2015-04-30 22:18:27 +0200

    Add new options to the front-end's home page

--- front/src/js/services/apiService.js
+++ front/src/js/services/apiService.js
@@ -5,13 +5,28 @@ apiService.factory('API', ['$location', 'Runs', 'Results', function($location, R
     return {
 
         launchTest: function(url, settings) {
-            Runs.save({
+            var runObject = {
                 url: url,
                 waitForResponse: false,
                 screenshot: true,
                 jsTimeline: true,
                 device: settings.device
-            }, function(data) {
+            };
+
+            if (settings.waitForSelector && settings.waitForSelector !== '') {
+                runObject.waitForSelector = settings.waitForSelector;
+            }
+
+            if (settings.cookie && settings.cookie !== '') {
+                runObject.cookie = settings.cookie;
+            }
+
+            if (settings.authUser && settings.authUser !== '' && settings.authPass && settings.authPass !== '') {
+                runObject.authUser = settings.authUser;
+                runObject.authPass = settings.authPass;
+            }
+
+            Runs.save(runObject, function(data) {
                 $location.path('/queue/' + data.runId);
             }, function(response) {
                 if (response.status === 429) {
