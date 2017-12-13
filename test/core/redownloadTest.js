a608ae47a48f70a9c60ba2278f5ddf69cb73bc50
Paul Hooijenga <paulhooijenga@gmail.com>
2017-12-13 15:54:36 +0100

    Use jimp instead of lwip for image processing
    Jimp does not install on recent versions of NodeJS because of changes
    in the bundled version of zlib. Jimp is a pure JS replacement.

--- test/core/redownloadTest.js
+++ test/core/redownloadTest.js
@@ -228,14 +228,14 @@ describe('redownload', function() {
             newEntry.weightCheck.bodySize.should.equal(4193);
             newEntry.weightCheck.bodyBuffer.should.deep.equal(fileContent);
 
-            // Opening the image in lwip to check if the format is good
-            var lwip = require('lwip');
-            lwip.open(newEntry.weightCheck.bodyBuffer, 'png', function(err, image) {
-                image.width().should.equal(620);
-                image.height().should.equal(104);
+            // Opening the image in jimp to check if the format is good
+            var Jimp = require('jimp');
+            Jimp.read(newEntry.weightCheck.bodyBuffer, function(err, image) {
+                image.bitmap.width.should.equal(620);
+                image.bitmap.height.should.equal(104);
                 done(err);
             });
-            
+
         })
 
         .fail(function(err) {
