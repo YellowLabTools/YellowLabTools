369512c6c13ed92a7e4afadefc9a1e1e87910a0e
Gaël Métais <gael.metais@gmail.com>
2018-01-16 21:06:18 -0600

    Remove Blanket coverage testing

--- test/blanket.js
+++
@@ -1,7 +0,0 @@
-var path = require('path');
-var srcDir = path.join(__dirname, '..', 'server');
-
-require('blanket')({
-  // Only files that match the pattern will be instrumented
-  pattern: srcDir
-});
