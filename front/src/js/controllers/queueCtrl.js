bd9cb2e25783b13572ed853be5c8d6b0d56cf473
Gaël Métais <gael.metais@gmail.com>
2021-01-03 01:43:46 +0000

    Add a progress bar on the UI

--- front/src/js/controllers/queueCtrl.js
+++ front/src/js/controllers/queueCtrl.js
@@ -9,14 +9,21 @@ queueCtrl.controller('QueueCtrl', ['$scope', '$routeParams', '$location', 'Runs'
         Runs.get({runId: $scope.runId}, function(data) {
             $scope.url = data.params.url;
             $scope.status = data.status;
+            $scope.progress = data.progress;
             $scope.notFound = false;
             $scope.connectionLost = false;
 
-            if (data.status.statusCode === 'running' || data.status.statusCode === 'awaiting') {
+            if (data.status.statusCode === 'awaiting') {
                 numberOfTries ++;
 
-                // Retrying in 2 seconds (and increasing the delay a bit more each time)
+                // Retrying every 2 seconds (and increasing the delay a bit more each time)
                 setTimeout(getRunStatus, 2000 + (numberOfTries * 100));
+
+            } else if (data.status.statusCode === 'running') {
+                numberOfTries ++;
+
+                // Retrying every second or so
+                setTimeout(getRunStatus, 1000 + (numberOfTries * 10));
 
             } else if (data.status.statusCode === 'complete') {
                 $location.path('/result/' + $scope.runId).replace();
